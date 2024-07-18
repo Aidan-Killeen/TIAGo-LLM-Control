78da99c34a0ac34cd200fb98d1bfdc71f06724e1
Aidan-Killeen <aidankkilleen@gmail.com>
2024-07-18 15:40:53 +0100

    Adjusted code - now causes the TIAGo simulation to move forwards when run

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # TIAGo-LLM-Control
 This code lets the user control a THIAGo Robot by using a LLM to interface with ROS
 
-To run, first catkin_make, then run "rosrun controls node"
+To run, first run catkin_make in the terminal with this directory open, then run "source devel/setup.bash"
+Then run "rosrun controls node"
 Ensure roscore has been opened in a different terminal first
--- src/controls/src/controls_node.cpp
+++ src/controls/src/controls_node.cpp
@@ -1,9 +1,27 @@
 #include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
 
 int main (int argc , char ** argv)
 {
-    ros::init(argc, argv , "hello_ros");
+    ros::init(argc, argv , "ROS_controls");
     ros::NodeHandle nh;
 
-    ROS_INFO_STREAM( "Hello, ROS!") ;
+
+    ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("mobile_base_controller/cmd_vel", 10000);//need the node name - should match rostopic list, without leading /'teleop_twist_keyboard'
+    
+
+    ros::Rate rate(2);
+    while(ros::ok())
+    {
+        geometry_msgs::Twist msg;
+        msg.linear.x = .5;
+        //msg.angular.x = 0;
+
+        pub.publish(msg);
+
+        ROS_INFO_STREAM( "Sending motion") ;
+        rate.sleep();
+    }
+
+    //ROS_INFO_STREAM( "Hello, ROS!") ;
 }

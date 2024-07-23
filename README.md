242bde0bfc3e27f13871fea4acdbdea8f83d432b
Aidan-Killeen <aidankkilleen@gmail.com>
2024-07-23 11:10:56 +0100

    Simple clarifications

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # TIAGo-LLM-Control
 This code lets the user control a THIAGo Robot by using a LLM to interface with ROS
 
+Before this code is lauched, a TIAGo simulation should be lauched first
+    Note: ensure tthe sinulation has "tuck_arm:=false", to avoid having to wait for the motion to finish playing
+
 To run, first run catkin_make in the terminal with this directory open, then run "source devel/setup.bash"
 Then run "rosrun controls node"
 Ensure roscore has been opened in a different terminal first
--- src/controls/src/controls_node.cpp
+++ src/controls/src/controls_node.cpp
@@ -7,17 +7,17 @@ int main (int argc , char ** argv)
     ros::NodeHandle nh;
 
 
-    ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("mobile_base_controller/cmd_vel", 10000);//need the node name - should match rostopic list, without leading /'teleop_twist_keyboard'
+    ros::Publisher base_pub = nh.advertise<geometry_msgs::Twist>("mobile_base_controller/cmd_vel", 10000);//need the node name - should match rostopic list, without leading /'teleop_twist_keyboard'
     
 
-    ros::Rate rate(2);
+    ros::Rate rate(2);  //rate is in hz - units per second
     while(ros::ok())
     {
-        geometry_msgs::Twist msg;
-        msg.linear.x = .5;
+        geometry_msgs::Twist base_var;
+        base_var.linear.x = .5;
         //msg.angular.x = 0;
 
-        pub.publish(msg);
+        base_pub.publish(base_var);
 
         ROS_INFO_STREAM( "Sending motion") ;
         rate.sleep();

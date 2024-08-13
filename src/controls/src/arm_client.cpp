e3700c524b8544ada7cf27173e1f5656dd25575f
Aidan-Killeen <aidankkilleen@gmail.com>
2024-08-13 16:23:43 +0100

    Legacy Code Removal

--- src/controls/src/arm_client.cpp
+++ src/controls/src/arm_client.cpp
@@ -20,104 +20,11 @@
 #include "controls/arm_position.h"
 
 
-typedef actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> arm_control_client;
-typedef boost::shared_ptr< arm_control_client>  arm_control_client_Ptr;
+//typedef actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> arm_control_client;
+//typedef boost::shared_ptr< arm_control_client>  arm_control_client_Ptr;
 
 //typedef actionlib::SimpleActionClient<pr2_controllers_msgs::Pr2GripperCommandAction> GripperClient;
 
-
-
-void createArmClient(arm_control_client_Ptr& actionClient)
-{
-    ROS_INFO("Creating action client to arm controller ...");
-    actionClient.reset(new arm_control_client("/arm_controller/follow_joint_trajectory"));    //this line causing issues in CMakee
-
-    int iterations = 0, max_iterations = 3;
-    while( !actionClient->waitForServer(ros::Duration(2.0)) && ros::ok() && iterations < max_iterations )
-    {
-        ROS_DEBUG("Waiting for the arm_controller_action server to come up");
-        iterations++;
-    }
-
-
-    if(iterations == max_iterations)
-    {
-        throw std::runtime_error("Error in createArmClient: arm controller action server not available");
-    }
-    
-
-
-}
-
-
-void waypoints_arm_goal(control_msgs::FollowJointTrajectoryGoal& goal)
-{
-    goal.trajectory.joint_names.push_back("arm_1_joint");
-    goal.trajectory.joint_names.push_back("arm_2_joint");
-    goal.trajectory.joint_names.push_back("arm_3_joint");
-    goal.trajectory.joint_names.push_back("arm_4_joint");
-    goal.trajectory.joint_names.push_back("arm_5_joint");
-    goal.trajectory.joint_names.push_back("arm_6_joint");
-    goal.trajectory.joint_names.push_back("arm_7_joint");
-
-    int waypoints = 1;
-
-    goal.trajectory.points.resize(waypoints);
-    for(int i=0; i< waypoints; i++)
-    {
-        goal.trajectory.points[i].positions.resize(7);
-
-        //set waypoint coords
-        goal.trajectory.points[i].positions[0] = 2.5;
-        for (int i1 = 1; i1 < 7; i1++)
-        {
-            goal.trajectory.points[i].positions[i1] = 1.0;
-        }
-
-        //set velocities
-        goal.trajectory.points[i].velocities.resize(7);
-        for (int j = 0; j < 7; ++j)
-        {
-            goal.trajectory.points[i].velocities[j] = 1.0;
-        }
-
-        goal.trajectory.points[i].time_from_start = ros::Duration(2.0);
-    }
-
-
-}
-
-
-int arm_joint_test()
-{
-
-    ROS_INFO_STREAM( "Starting arm joint test...") ;
-    
-    ros::NodeHandle nh;
-    if (!ros::Time::waitForValid(ros::WallDuration(10.0)))
-    {
-        ROS_FATAL("Timed-out waiting for valid time.");
-        return EXIT_FAILURE;
-    }
-    
-    arm_control_client_Ptr ArmClient;
-    createArmClient(ArmClient);
-    
-    control_msgs::FollowJointTrajectoryGoal arm_goal;
-    waypoints_arm_goal(arm_goal);
-    
-    arm_goal.trajectory.header.stamp = ros::Time::now() + ros::Duration(1.0);
-    ArmClient->sendGoal(arm_goal);
-
-    while(!(ArmClient->getState().isDone()) && ros::ok())
-    {
-        ros::Duration(4).sleep(); // sleep for four seconds
-    }
-    
-    ROS_INFO_STREAM( "Arm joint test completed") ;
-    return EXIT_SUCCESS;
-
-}
 
 int move_it_arm(std::map<std::string, double> target_position)
 {
@@ -179,33 +86,20 @@ int move_it_arm(std::map<std::string, double> target_position)
 
 void motionCallback(const controls::arm_position::ConstPtr& msg)
 {
-
-    //control_msgs::FollowJointTrajectoryGoal arm_goal;
-    //ROS_INFO("I heard: [%s]", msg->data.c_str());
     ROS_INFO("Message Recieved");
-    //double[] coords = msg -> coords;
+
 
     std::map<std::string, double> target_position;
-    /**
-    target_position["torso_lift_joint"] = msg->coords[0];//0.0;//atof(argv[1]);
-    target_position["arm_1_joint"] = msg->coords[1];//2.5;//atof(argv[2]);
-    target_position["arm_2_joint"] = msg->coords[2];//1.0;//atof(argv[3]);
-    target_position["arm_3_joint"] = msg->coords[3];//1.0;;//atof(argv[4]);
-    target_position["arm_4_joint"] = msg->coords[4];//1.0;//atof(argv[5]);
-    target_position["arm_5_joint"] = msg->coords[5];//1.0;//atof(argv[6]);
-    target_position["arm_6_joint"] = msg->coords[6];//1.0;//atof(argv[7]);
-    target_position["arm_7_joint"] = msg->coords[7];//1.0;//atof(argv[8]);
-    */
-    target_position["torso_lift_joint"] = msg->torso_lift_joint;//0.0;//atof(argv[1]);
-    target_position["arm_1_joint"] = msg->arm_1_joint;//2.5;//atof(argv[2]);
-    target_position["arm_2_joint"] = msg->arm_2_joint;//1.0;//atof(argv[3]);
-    target_position["arm_3_joint"] = msg->arm_3_joint;//1.0;;//atof(argv[4]);
-    target_position["arm_4_joint"] = msg->arm_4_joint;//1.0;//atof(argv[5]);
-    target_position["arm_5_joint"] = msg->arm_5_joint;//1.0;//atof(argv[6]);
-    target_position["arm_6_joint"] = msg->arm_6_joint;//1.0;//atof(argv[7]);
-    target_position["arm_7_joint"] = msg->arm_7_joint;//1.0;//atof(argv[8]);
-    //int num = atoi(msg->data.c_str());
-    //ROS_INFO_STREAM("Motion duration: " << num
+
+    target_position["torso_lift_joint"] = msg->torso_lift_joint;
+    target_position["arm_1_joint"] = msg->arm_1_joint;
+    target_position["arm_2_joint"] = msg->arm_2_joint;
+    target_position["arm_3_joint"] = msg->arm_3_joint;
+    target_position["arm_4_joint"] = msg->arm_4_joint;
+    target_position["arm_5_joint"] = msg->arm_5_joint;
+    target_position["arm_6_joint"] = msg->arm_6_joint;
+    target_position["arm_7_joint"] = msg->arm_7_joint;
+
     move_it_arm(target_position);
     
     
@@ -227,24 +121,8 @@ int main(int argc, char** argv)
     }
     
 
-    //arm_joint_test(); //basic controls
-    /*
-    std::map<std::string, double> target_position;
-    target_position["torso_lift_joint"] = 0.0;//atof(argv[1]);
-    target_position["arm_1_joint"] = 2.5;//atof(argv[2]);
-    target_position["arm_2_joint"] = 1.0;//atof(argv[3]);
-    target_position["arm_3_joint"] = 1.0;;//atof(argv[4]);
-    target_position["arm_4_joint"] = 1.0;//atof(argv[5]);
-    target_position["arm_5_joint"] = 1.0;//atof(argv[6]);
-    target_position["arm_6_joint"] = 1.0;//atof(argv[7]);
-    target_position["arm_7_joint"] = 1.0;//atof(argv[8]);
-    move_it_arm(target_position);//using moveit instead
-    */
-    //set up listener instead
-
     ros::Subscriber sub = nh.subscribe("control_msgs",1000, motionCallback);
-    //ros::Subscriber sub = nh.subscribe("chatter", 1000, motionCallback);
-    //ros::Subscriber sub = nh.subscribe("chatter", 1000, chatterCallback);
+
     ROS_INFO("Waiting for Msg ...");
 
     ros::Rate rate(2);
